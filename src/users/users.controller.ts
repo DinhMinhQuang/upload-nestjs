76dee0a7c21f903bcd1352a884f1a214fe45ac2f
Minh Quang <letuyettrinh10c12@gmail.com>
2024-08-30 01:00:02 +0700

    Need go to sleep

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,14 +11,19 @@ import {
 import { UsersService } from './users.service';
 import { Request, Response } from 'express';
 import { CreateUserDto } from './dto/create-user.dto';
+import { FindOneParams } from './dto/findOne-params.dto';
 
 @Controller({ path: 'users', scope: Scope.REQUEST })
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Get(':id')
-  async findOne(@Param('id') id: string, @Res() res: Response): Promise<any> {
-    const test = await this.usersService.findOne(id);
+  async findOne(
+    @Param() params: FindOneParams,
+    @Res() res: Response,
+  ): Promise<any> {
+    console.log(params);
+    const test = await this.usersService.findOne(params.id);
     console.log(test);
     res.status(HttpStatus.CREATED).send({ code: 1, message: 'Succeeded' });
   }
